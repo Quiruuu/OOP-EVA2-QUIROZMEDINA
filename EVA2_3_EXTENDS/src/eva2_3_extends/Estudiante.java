6743f0a9e1eb8cb1c71e824ba0431e8084cf8dc8
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-03-08 12:28:39 -0600

    Add files via upload

---
+++ EVA2_3_EXTENDS/src/eva2_3_extends/Estudiante.java
@@ -0,0 +1,16 @@
+package eva2_3_extends;
+            //CLASE DERIVADA extends CLASE BASE
+            //CLASE HIJO extends CLASE PADRE
+            //SUBCLASE extends SUPERCLASE
+public class Estudiante extends Persona {
+    private String noControl;
+
+    public String getNoControl() {
+        return noControl;
+    }
+
+    public void setNoControl(String noControl) {
+        this.noControl = noControl;
+    }
+    
+}
