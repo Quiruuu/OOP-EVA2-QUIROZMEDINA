f8520a08e71880c3a017e55a7bbd876505afa7b4
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-03-16 12:25:58 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,16 @@
+package eva2_12_clases_abstractas;
+public class EVA2_12_CLASES_ABSTRACTAS {
+    public static void main(String[] args) {
+       Prueba prueba = new Prueba(); 
+       PruebaAbstracta pruebaabstracta = new PruebaAbstracta();
+    }
+    
+}
+    
+class Prueba{
+
+}
+//NO SE PUEDE INSTANCIAR
+abstract class PruebaAbstracta{
+    
+}
