da7691292d3c6019f9e462f6b2432c95e9c1ab64
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-04-23 20:19:11 -0600

    Add files via upload

---
+++ EVA2_15_PROTECTED/src/Prueba/SuperClase.java
@@ -0,0 +1,13 @@
+package Prueba;
+public class SuperClase {
+    protected int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+}
